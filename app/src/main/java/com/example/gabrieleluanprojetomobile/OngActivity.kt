0d249818a01f536a6418765145ee9f7632e9395e
Gabriel Santana <gabrielsantana77@hotmail.com>
2021-06-20 22:02:59 -0300

    databind

--- app/src/main/java/com/example/gabrieleluanprojetomobile/OngActivity.kt
+++ app/src/main/java/com/example/gabrieleluanprojetomobile/OngActivity.kt
@@ -1,23 +1,21 @@
 package com.example.gabrieleluanprojetomobile
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import com.example.gabrieleluanprojetomobile.databinding.ActivityOutraBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding = ActivityOngBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
-    binding.actionButton.setOnClickListener {
-        val intent = Intent(this, OutraActivity::class.java)
-        //intent.putExtra("MENSAGEM", "Olá!")
-        //intent.putExtra("NUMERO", 42)
-        startActivity(intent)
-    }
 }
 
 
